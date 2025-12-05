ed7c31f89b8f5e465a0ed5fede00db4c7b0d85da
Феликс Ким <undercast326@gmail.com>
2025-12-05 12:41:02 +0500

    Закончил написание моделей

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Major(models.Model):
+    name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+
+class University(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    description = models.TextField()
+    min_unt_score = models.IntegerField()
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    languages = models.CharField(max_length=400)
+    majors = models.ManyToManyField(Major, related_name='universities')
+    tuition_fee = models.IntegerField()
+    internships = models.BooleanField(default=True)
+    scholarships = models.BooleanField(default=True)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return f'{self.name} - {self.city}'
