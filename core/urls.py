a79f823a5fce62b5628e306317e44620dfa6ece1
Феликс Ким <undercast326@gmail.com>
2025-12-05 14:28:10 +0500

    Сделай urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from webapp import views
+
+router = DefaultRouter()
+router.register('majors', views.MajorViewSet)
+router.register('universities', views.UniversityViewSet)
+router.register('types', views.TypeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('', include(router.urls))
 ]
