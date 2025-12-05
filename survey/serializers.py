37dca0619dff9cdbe3662c1250f26ad738b0cabd
Феликс Ким <undercast326@gmail.com>
2025-12-05 15:48:49 +0500

    Написал сериализатор

--- survey/serializers.py
+++ survey/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Questions, Choice
+from .models import Questions, Choice, SurveyResponse
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -14,3 +14,10 @@ class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields ='__all__'
+
+
+class SurveyResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SurveyResponse
+        fields = ('id', 'responses_data', 'completed_at', 'ai_analysis')
+        read_only_fields = ('id', 'completed_at', 'ai_analysis')
